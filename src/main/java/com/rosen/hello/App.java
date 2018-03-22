8e3c65e0de795c45b3adc266bcf328ea9734dafa
Rosen Silva <rosen@live.com>
2018-03-22 13:44:35 +0530

    add hello world sample for testing with travis/jenkins

---
+++ src/main/java/com/rosen/hello/App.java
@@ -0,0 +1,13 @@
+package com.rosen.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
